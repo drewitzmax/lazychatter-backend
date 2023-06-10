279a2668c17c0b8c7046944aa35dd88df1844a6a
drewitzmax <55143588+drewitzmax@users.noreply.github.com>
2023-06-10 12:25:25 +0200

    use chat object instead of id as id will not be generated by then on new chats

--- src/main/java/at/ac/fhcampuswien/lazychatter/service/ChatService.java
+++ src/main/java/at/ac/fhcampuswien/lazychatter/service/ChatService.java
@@ -5,5 +5,5 @@ import at.ac.fhcampuswien.lazychatter.model.jpa.Chat;
 public interface ChatService {
     Chat createNewChat(String ownName, String[] participants);
 
-    Chat addUserToChat(String participant, String chatId);
+    Chat addUserToChat(String participant, Chat chat);
 }
--- src/main/java/at/ac/fhcampuswien/lazychatter/service/ChatServiceImpl.java
+++ src/main/java/at/ac/fhcampuswien/lazychatter/service/ChatServiceImpl.java
@@ -18,15 +18,14 @@ public class ChatServiceImpl implements ChatService{
         Chat chat = new Chat();
         chat.addParticipant(userRepository.getUserByUsername(ownName));
         for(String participant : participants)
-        addUserToChat(participant, chat.getId());
+        addUserToChat(participant, chat);
         chat = chatRepository.saveAndFlush(chat);
         return chat;
     }
 
     @Override
-    public Chat addUserToChat(String participant, String chatId) {
-        Chat chat = chatRepository.findById(chatId).get();
-            chat.addParticipant(userRepository.getUserByUsername(participant));
+    public Chat addUserToChat(String participant, Chat chat) {
+        chat.addParticipant(userRepository.getUserByUsername(participant));
         return chat;
     }
 }
